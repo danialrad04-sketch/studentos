a88321a806f6ea52a942f846202956370fd65882
danial <danialrad04@gmail.com>
2026-09-24 16:56:24 +0330

    test: pin Robolectric to SDK 35

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -20,7 +20,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [36])
+@Config(sdk = [35])
 class ExampleRobolectricTest {
 
   private lateinit var db: AppDatabase
