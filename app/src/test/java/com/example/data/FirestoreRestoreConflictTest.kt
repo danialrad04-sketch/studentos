a20260dca7245e6579c98c2f929921449ae848d1
danial <danialrad04@gmail.com>
2026-09-24 16:56:43 +0330

    test: pin Robolectric to SDK 35

--- app/src/test/java/com/example/data/FirestoreRestoreConflictTest.kt
+++ app/src/test/java/com/example/data/FirestoreRestoreConflictTest.kt
@@ -18,7 +18,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [36])
+@Config(sdk = [35])
 class FirestoreRestoreConflictTest {
 
     private lateinit var db: AppDatabase
