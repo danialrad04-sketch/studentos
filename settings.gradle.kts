f418362ea03d13bd14e7d5a9dd2c43485481ea9c
danial <danialrad04@gmail.com>
2026-09-24 13:35:43 +0330

    build: use stable Maven Google endpoint

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
     }
   }
   repositories {
-    google()
+    maven { url = uri("https://maven.google.com") }
     mavenCentral()
     gradlePluginPortal()
   }
@@ -20,7 +20,7 @@ plugins {
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
-    google()
+    maven { url = uri("https://maven.google.com") }
     mavenCentral()
   }
 }
