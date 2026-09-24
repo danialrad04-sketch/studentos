e238cea14287a16927055569f2310c2b10bce9f6
danial <danialrad04@gmail.com>
2026-09-24 16:56:36 +0330

    test: pin Robolectric to SDK 35

--- app/src/test/java/com/example/data/DataPersistenceAndBackupTest.kt
+++ app/src/test/java/com/example/data/DataPersistenceAndBackupTest.kt
@@ -26,7 +26,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [36])
+@Config(sdk = [35])
 class DataPersistenceAndBackupTest {
 
     private lateinit var db: AppDatabase
